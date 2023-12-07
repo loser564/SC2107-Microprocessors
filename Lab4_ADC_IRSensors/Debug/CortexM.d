17fd399c9e65962cc80a9a3dbfbf4e8e542ded08
Alicia Lam <66523331+loser564@users.noreply.github.com>
2023-12-08 00:35:24 +0800

    Add files via upload

---
+++ Lab4_ADC_IRSensors/Debug/CortexM.d
@@ -0,0 +1,7 @@
+# FIXED
+
+CortexM.obj: C:/Users/h2user/Desktop/round\ 8/CE2007/inc/CortexM.c
+CortexM.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h
+
+C:/Users/h2user/Desktop/round\ 8/CE2007/inc/CortexM.c: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h: 
